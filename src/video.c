2f29f2818089a8da5794d701f350a0c81169aea7
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2024-11-23 12:40:54 -0800

    attempt to the textures

--- src/video.c
+++ src/video.c
@@ -1,12 +1,22 @@
 #include "video.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+
+float image[] = {
+    1,0,0,
+    0,1,0,
+    0,0,1,
+    0.5,0,0.5
+};
 
 void init_ogl(int wid, int hei) {
     glViewport(0, 0, wid, hei);
     glClearColor(0.0, 0.0, 0.0, 1.0);
-    //glEnable(GL_TEXTURE_2D);
-    //glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_DEPTH_TEST);
 
     const float DEG2RAD = SDL_acos(-1.0f) / 180;
 	const float front = 0.1;
@@ -26,19 +36,45 @@ void init_ogl(int wid, int hei) {
 
     glMatrixMode(GL_PROJECTION);
     glFrustum(-right, right, -top, top, front, back);
+
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glActiveTexture(GL_TEXTURE0);
+    uint tex_id = 0;
+    glGenTextures(1, &tex_id);
+    glBindTexture( GL_TEXTURE_2D, tex_id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGB, GL_FLOAT, image);
+
+    glFinish();
 }
 
 void do_ogl_updates(void) {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0, 0);
-    glVertex3f(0, 0.3,-3);
-    glColor3f(0, 1.0, 0);
-    glVertex3f(-0.33, -0.3,-3);
-    glColor3f(0, 0, 1.0);
-    glVertex3f(0.33, -0.3,-3);
+    glActiveTexture(GL_TEXTURE0);
+    glBegin(GL_TRIANGLE_STRIP);
+    //glColor3f(1.0, 0, 0);
+    glTexCoord2f(0, 0);
+    glVertex3f(-0.33, 0.3,-1);
+    //glColor3f(0, 1.0, 0);
+    glTexCoord2f(1, 0);
+    glVertex3f(-0.33, -0.3,-1);
+    //glColor3f(0, 0, 1.0);
+    glTexCoord2f(1, 1);
+    glVertex3f(0.33, 0.3,-1);
+    //glColor3f(1.0,0,1.0);
+    glTexCoord2f(0, 1);
+    glVertex3f(0.33, -0.3,-1);
     glEnd();
+
+    glFinish();
     
+    uint err = glGetError();
+    if (err != GL_NO_ERROR) {
+        dprintf(2, "oh shit! gl errored with %i! that's bad!\n", err);
+        exit(-1);
+    }
 }
 
